d2401fa02fd438b900fe7a6072854fc7508fda3d
Bowie <beatrizvsgoncalves.274@gmail.com>
2024-03-26 21:47:39 -0300

    Create ambiente.js

---
+++ 13-loopings/ambiente.js
@@ -0,0 +1,22 @@
+var contador = 1
+
+do { 
+    console.log(`Passo - ${contador}`);
+    contador++
+} while (contador <= 6)
+
+/* 
+while (contador <= 6) {
+    console.log(`Passo - ${contador}`);
+    contador++ // contador = contador + 1
+} 
+*/
+
+/*
+console.log("Tudo Bem?")
+console.log("Tudo Bem?")
+console.log("Tudo Bem?")
+console.log("Tudo Bem?")
+console.log("Tudo Bem?")
+console.log("Tudo Bem?")
+*/
