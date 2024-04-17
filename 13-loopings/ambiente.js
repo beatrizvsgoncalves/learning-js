d57a7378ce5128f1a1173b6f4e217a13cb5f4cc9
Bowie <beatrizvsgoncalves.274@gmail.com>
2024-04-16 21:27:00 -0300

    Modificações
    Retirada de divisores no ambiente.js, criação do testesFor.js e do launch.json (para depuração).

--- 13-loopings/ambiente.js
+++ 13-loopings/ambiente.js
@@ -4,21 +4,15 @@ while (contWhile <= 5) {
     contWhile++ // contWhile = contWhile + 1
 }
 
-//--------------
-
 var contDo = 6
 do { 
     console.log(`Passo - ${contDo}`);
     contDo++
 } while (contDo <= 10)
 
-//--------------
-
 for (var contFor = 11; contFor <= 15; contFor++) {
     console.log(`Passo - ${contFor}`);
 }
-
-//--------------
 
 /*
     console.log("Passo - 1")
