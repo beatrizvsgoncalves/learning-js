0fd80c00c6798d95abd0f46c8b0e2c5b35c216f2
Bowie <beatrizvsgoncalves.274@gmail.com>
2024-04-26 16:55:54 -0300

    ajustes

--- final-exercises/number-analyzer/script.js
+++ final-exercises/number-analyzer/script.js
@@ -2,11 +2,13 @@ let botaoAdicionar = document.querySelector("input#botao-adicionar");
 botaoAdicionar.addEventListener("click", adicionarNumero);
 
 function adicionarNumero() {
-	var num = document.querySelector("input#txtnum");
+	let num = document.querySelector("input#txtnum");
 	let listaAdicionados = document.querySelector("select#lista");
 
-	if (num.value.length != 0 && n >= 1 && n <= 100) {
+	if (num.value.length != 0 && num.value >= 1 && num.value <= 100) {
 		var n = [Number(num.value)];
+		n.sort()
+
 		let item = document.createElement("option");
 		item.text = `Valor ${n} adicionado`;
 		item.value = "numero";
@@ -17,18 +19,29 @@ function adicionarNumero() {
 		window.alert("Valor inválido ou já encontrado na lista!");
 	}
 
-	/* 
+	
 	let botaoFinalizar = document.querySelector("input#botao-finalizar");
 	botaoFinalizar.addEventListener("click", finalizar);
+
 	function finalizar() {
         if (num.value.length != 0) {
-            n.sort()
-            let msg = document.querySelector("div#msg")
-            msg.innerHTML = `<p>Ao todo, temos ${n.length} números cadastrados.</p>`
-            msg.innerHTML += `<p>O maior valor informado foi ${n.indexOf(n.length)}.</p>`
+			let soma = 0
+			for (let i = 0; i < n.length; i++) {
+				soma += n[i]
+			}
 
+			let media = soma / 2
+
+			let msg = document.querySelector("div#msg")
+				msg.innerHTML = `
+				<p>Ao todo, temos ${n.length} números cadastrados.</p>
+				<p>O maior valor informado foi ${n[n.length - 1]}.</p>
+				<p>O menor valor informado foi ${n[0]}.</p>
+				<p>Somando todos os valores, temos ${soma}.
+				<p>A média dos valores digitados é ${media}.
+				`
         } else {
             window.alert("Adicione números antes de finalizar!")
         }
-    } */
+    }
 }
